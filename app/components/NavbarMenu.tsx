93a4fad6adb70bba608eb5340c66cae3191984f8
Sal <salmanawal2012@gmail.com>
2025-07-16 17:32:11 +1000

    updated navBar burger menu

--- app/components/NavbarMenu.tsx
+++ app/components/NavbarMenu.tsx
@@ -9,7 +9,7 @@ import {
   Collapse,
   Box,
   ListItemButton,
-  Link,
+  Link as MuiLink,
 } from "@mui/material";
 import ExpandLess from "@mui/icons-material/ExpandLess";
 import ExpandMore from "@mui/icons-material/ExpandMore";
@@ -19,6 +19,11 @@ const NavbarMenu: React.FC = () => {
   const [open, setOpen] = React.useState(false);
   const [openInfo, setOpenInfo] = React.useState(false);
 
+  const closeMenu = () => {
+    setOpen(false);
+    setOpenInfo(false);
+  };
+
   const linkStyle = {
     textDecoration: "none",
     textTransform: "none",
@@ -26,25 +31,35 @@ const NavbarMenu: React.FC = () => {
   };
 
   return (
-    <Box>
-      {/* Hamburger Icon */}
-      <Box sx={{ position: "fixed", top: 16, left: 16, zIndex: 1301 }}>
+    <>
+      {/* Always render single hamburger (animated) */}
+      <div
+        style={{
+          position: "fixed",
+          top: 16,
+          left: 16,
+          zIndex: 1500,
+        }}
+      >
         <Hamburger
           toggled={open}
           toggle={setOpen}
           direction="right"
-          color="black"
+          color="#615252"
+          size={24}
         />
-      </Box>
+      </div>
 
-      {/* Navigation Modal */}
+      {/* Modal */}
       <Modal
         open={open}
-        onClose={() => setOpen(false)}
+        onClose={closeMenu}
+        disablePortal // ✅ keeps it inside current DOM hierarchy
         closeAfterTransition
         slotProps={{
           backdrop: { timeout: 300 },
         }}
+        disableAutoFocus
       >
         <Fade in={open} timeout={400}>
           <Box
@@ -53,18 +68,19 @@ const NavbarMenu: React.FC = () => {
               height: "100vh",
               bgcolor: "white",
               p: 5,
-              pt: 10, // padding top to avoid overlapping hamburger
-              outline: 0,
+              pt: 10,
               position: "relative",
               overflowY: "auto",
               color: "#615252",
+              zIndex: 1400, // Lower than hamburger
             }}
           >
             <List sx={{ ml: "50px" }}>
               <ListItemButton
-                component={Link}
+                component={MuiLink}
                 href="/portfolio"
-                sx={{ ...linkStyle }}
+                sx={linkStyle}
+                onClick={closeMenu}
               >
                 <ListItemText primary="Portfolio" />
               </ListItemButton>
@@ -77,23 +93,26 @@ const NavbarMenu: React.FC = () => {
               <Collapse in={openInfo} timeout="auto" unmountOnExit>
                 <List component="div" disablePadding>
                   <ListItemButton
-                    component={Link}
+                    component={MuiLink}
                     href="/the-experience"
                     sx={{ ...linkStyle, pl: 4 }}
+                    onClick={closeMenu}
                   >
                     <ListItemText primary="The Experience" />
                   </ListItemButton>
                   <ListItemButton
-                    component={Link}
+                    component={MuiLink}
                     href="/faqs"
                     sx={{ ...linkStyle, pl: 4 }}
+                    onClick={closeMenu}
                   >
                     <ListItemText primary="FAQs" />
                   </ListItemButton>
                   <ListItemButton
-                    component={Link}
+                    component={MuiLink}
                     href="/about"
                     sx={{ ...linkStyle, pl: 4 }}
+                    onClick={closeMenu}
                   >
                     <ListItemText primary="Meet the Photographer" />
                   </ListItemButton>
@@ -101,17 +120,19 @@ const NavbarMenu: React.FC = () => {
               </Collapse>
 
               <ListItemButton
-                component={Link}
+                component={MuiLink}
                 href="/pricing"
-                sx={{ ...linkStyle }}
+                sx={linkStyle}
+                onClick={closeMenu}
               >
                 <ListItemText primary="Pricing" />
               </ListItemButton>
 
               <ListItemButton
-                component={Link}
+                component={MuiLink}
                 href="/contact"
-                sx={{ ...linkStyle }}
+                sx={linkStyle}
+                onClick={closeMenu}
               >
                 <ListItemText primary="Contact" />
               </ListItemButton>
@@ -119,7 +140,7 @@ const NavbarMenu: React.FC = () => {
           </Box>
         </Fade>
       </Modal>
-    </Box>
+    </>
   );
 };
 
