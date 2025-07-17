0eaef9fcfd187598fdabf88701f05423fbcae4ec
Sal <salmanawal2012@gmail.com>
2025-07-18 01:32:22 +1000

    dynamic hero updated

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div className="relative w-full aspect-[8/5] max-h-[95vh] max-w-[2160px] mx-auto">
+      <div className="relative w-full aspect-[8/5] max-h-[110vh] max-w-[2160px] mx-auto">
         <Image
           src="/Home.jpg"
           alt="Couple holding hands together in front of a big tree"
           width={2160}
           height={1350}
-          className="w-full max-h-[95vh] object-cover"
+          className="w-full max-h-[110vh] object-cover"
           style={{ objectPosition: "50% 60%" }}
           priority
         />
