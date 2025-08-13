9a7f3ddb452a575c4267351cad50fcc5df7ecf3b
Sal <salmanawal2012@gmail.com>
2025-08-13 14:20:37 +1000

    client login modals front end complete

--- app/components/UI/LogInModal.tsx
+++ app/components/UI/LogInModal.tsx
@@ -15,6 +15,32 @@ export default function LogInModal() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+
+  function Spinner({ color = "black" }) {
+    return (
+      <svg
+        className={`animate-spin h-5 w-5 text-${color}`}
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <circle
+          className="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          strokeWidth="4"
+        ></circle>
+        <path
+          className="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+        ></path>
+      </svg>
+    );
+  }
+
   return (
     <>
       <Modal
@@ -23,7 +49,7 @@ export default function LogInModal() {
         className="flex justify-center items-center"
       >
         {/* Modal content */}
-        <div className="relative flex flex-col items-center w-full h-full bg-white sm:h-[600px] sm:w-[380px] sm:rounded-xl outline-none p-6">
+        <div className="relative flex flex-col items-center w-full h-full bg-white sm:h-[600px] sm:w-[380px] sm:rounded-xl outline-none border-none p-6">
           {/* Close button */}
           <button
             onClick={() => dispatch(closeModal())}
@@ -34,10 +60,10 @@ export default function LogInModal() {
           </button>
 
           {/* Title */}
-          <h2 className="mt-6 text-4xl font-bold mb-8">Log In</h2>
+          <h2 className="mt-20 sm:mt-5 text-4xl font-bold mb-8">Log In</h2>
 
           <div className="flex flex-col items-center space-y-4 w-full">
-            <button className="flex flex-row items-center w-[250px] h-10 rounded-2xl text-[16px] font-medium cursor-pointer ">
+            <button className="flex flex-row items-center w-[250px] h-10 rounded-2xl text-[16px] font-medium cursor-pointer hover:-translate-y-1 transition ">
               <div className="flex justify-center items-center w-10">
                 <Image
                   src="/google.svg"
@@ -50,7 +76,7 @@ export default function LogInModal() {
               <span className="flex-1 text-left pl-2">Log In With Google</span>
             </button>
 
-            <button className="flex flex-row items-center w-[250px] h-10 rounded-2xl text-[16px] font-medium cursor-pointer ">
+            <button className="flex flex-row items-center w-[250px] h-10 rounded-2xl text-[16px] font-medium cursor-pointer hover:-translate-y-1 transition">
               <div className="flex justify-center items-center w-10">
                 <Image
                   src="/apple.svg"
@@ -78,24 +104,24 @@ export default function LogInModal() {
               <input
                 type="email"
                 placeholder="your@email.com"
-                className="mx-auto border border-gray-300 rounded-lg px-4 py-2 focus:outline-none w-[250px]"
+                className="mx-auto border border-gray-300 rounded-lg px-4 py-2 focus:outline-none w-[85%] sm:max-w-[250px]"
               />
             </div>
             {/* Password field */}
             <div className="flex flex-col gap-2 w-full">
               <label className="text-left">Password</label>
-              <div className="relative w-[250px] mx-auto">
+              <div className="relative w-[85%] sm:max-w-[250px] mx-auto">
                 <input
                   type={showPassword ? "text" : "password"}
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                   placeholder="your password"
-                  className="border border-gray-300 rounded-lg px-4 py-2 focus:outline-none w-full pr-10"
+                  className="border w-full border-gray-300 rounded-lg px-4 py-2 focus:outline-none pr-10"
                   autoComplete="current-password"
                 />
                 <button
                   type="button"
-                  className="absolute top-1/2 right-3 transform -translate-y-1/2 text-gray-400 hover:text-gray-600 cursor-pointer"
+                  className="absolute top-1/2 right-4 transform -translate-y-1/2 text-gray-400 hover:text-gray-600 cursor-pointer"
                   aria-label={showPassword ? "Hide password" : "Show password"}
                   tabIndex={0}
                   onClick={() => setShowPassword((s) => !s)}
@@ -108,12 +134,18 @@ export default function LogInModal() {
                 </button>
               </div>
             </div>
+            <button
+              className="text-right -mt-1 mb-1 text-[#ea992f] font-semibold cursor-pointer"
+              onClick={() => console.log("forgot password function ran")}
+            >
+              Forgot your Password?
+            </button>
           </div>
 
-          <button className="mt-6 w-[90%] text-xl font-semibold h-[50px] bg-[#f5dec0] cursor-pointer rounded-full">
+          <button className="mt-2 w-[90%] text-xl font-semibold h-[50px] bg-[#f5dec0] cursor-pointer rounded-full">
             Log In
           </button>
-          <div className="text-sm mt-4 flex gap-2">
+          <div className="text-sm mt-2 flex gap-2">
             Don't have an account yet?
             <span
               className="text-[#ea992f] font-bold underline cursor-pointer"
--- app/components/UI/SignUpModal.tsx
+++ app/components/UI/SignUpModal.tsx
@@ -17,6 +17,32 @@ export default function SignUpModal() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+
+  function Spinner({ color = "black" }) {
+    return (
+      <svg
+        className={`animate-spin h-5 w-5 text-${color}`}
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <circle
+          className="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          strokeWidth="4"
+        ></circle>
+        <path
+          className="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+        ></path>
+      </svg>
+    );
+  }
+
   return (
     <Modal
       open={isOpen}
@@ -35,10 +61,10 @@ export default function SignUpModal() {
         </button>
 
         {/* Title */}
-        <h2 className="mt-6 text-4xl font-bold mb-8">Sign Up</h2>
+        <h2 className="mt-20 sm:mt-6 text-4xl font-bold mb-8">Sign Up</h2>
 
         <div className="flex flex-col items-center space-y-4 w-full">
-          <button className="flex flex-row items-center w-[250px] h-10 rounded-2xl text-[16px] font-medium cursor-pointer ">
+          <button className="flex flex-row items-center w-[250px] h-10 rounded-2xl text-[16px] font-medium cursor-pointer hover:-translate-y-1 transition">
             <div className="flex justify-center items-center w-10">
               <Image
                 src="/google.svg"
@@ -51,7 +77,7 @@ export default function SignUpModal() {
             <span className="flex-1 text-left pl-2">Sign Up With Google</span>
           </button>
 
-          <button className="flex flex-row items-center w-[250px] h-10 rounded-2xl text-[16px] font-medium cursor-pointer ">
+          <button className="flex flex-row items-center w-[250px] h-10 rounded-2xl text-[16px] font-medium cursor-pointer hover:-translate-y-1 transition ">
             <div className="flex justify-center items-center w-10">
               <Image src="/apple.svg" height={18} width={18} alt="Apple Logo" />
             </div>
@@ -74,13 +100,13 @@ export default function SignUpModal() {
             <input
               type="email"
               placeholder="your@email.com"
-              className="mx-auto border border-gray-300 rounded-lg px-4 py-2 focus:outline-none w-[250px]"
+              className="mx-auto border border-gray-300 rounded-lg px-4 py-2 focus:outline-none w-[85%] sm:max-w-[250px]"
             />
           </div>
           {/* Password field */}
           <div className="flex flex-col gap-2 w-full">
             <label className="text-left">Password</label>
-            <div className="relative w-[250px] mx-auto">
+            <div className="relative w-[85%] sm:max-w-[250px] mx-auto">
               <input
                 type={showPassword ? "text" : "password"}
                 value={password}
@@ -91,7 +117,7 @@ export default function SignUpModal() {
               />
               <button
                 type="button"
-                className="absolute top-1/2 right-3 transform -translate-y-1/2 text-gray-400 hover:text-gray-600 cursor-pointer"
+                className="absolute top-1/2 right-4 transform -translate-y-1/2 text-gray-400 hover:text-gray-600 cursor-pointer"
                 aria-label={showPassword ? "Hide password" : "Show password"}
                 tabIndex={0}
                 onClick={() => setShowPassword((s) => !s)}
