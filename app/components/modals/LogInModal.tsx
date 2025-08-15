59e9459c652d1543f2b3c158ea34333288033b8e
Sal <salmanawal2012@gmail.com>
2025-08-15 11:53:08 +1000

    typescript errors fixed

--- app/components/modals/LogInModal.tsx
+++ app/components/modals/LogInModal.tsx
@@ -16,30 +16,30 @@ export default function LogInModal() {
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
 
-  function Spinner({ color = "black" }) {
-    return (
-      <svg
-        className={`animate-spin h-5 w-5 text-${color}`}
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <circle
-          className="opacity-25"
-          cx="12"
-          cy="12"
-          r="10"
-          stroke="currentColor"
-          strokeWidth="4"
-        ></circle>
-        <path
-          className="opacity-75"
-          fill="currentColor"
-          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-        ></path>
-      </svg>
-    );
-  }
+  // function Spinner({ color = "black" }) {
+  //   return (
+  //     <svg
+  //       className={`animate-spin h-5 w-5 text-${color}`}
+  //       xmlns="http://www.w3.org/2000/svg"
+  //       fill="none"
+  //       viewBox="0 0 24 24"
+  //     >
+  //       <circle
+  //         className="opacity-25"
+  //         cx="12"
+  //         cy="12"
+  //         r="10"
+  //         stroke="currentColor"
+  //         strokeWidth="4"
+  //       ></circle>
+  //       <path
+  //         className="opacity-75"
+  //         fill="currentColor"
+  //         d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+  //       ></path>
+  //     </svg>
+  //   );
+  // }
 
   return (
     <>
@@ -103,6 +103,9 @@ export default function LogInModal() {
               <label className="text-left">Email Address</label>
               <input
                 type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                autoComplete="email"
                 placeholder="your@email.com"
                 className="mx-auto border border-gray-300 rounded-lg px-4 py-2 focus:outline-none w-[85%] sm:max-w-[250px]"
               />
--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,9 +1,5 @@
 import React from "react";
 
 export default function page() {
-  return (
-    <>
-      <span>Welcome to the pricing page</span>
-    </>
-  );
+  return <></>;
 }
